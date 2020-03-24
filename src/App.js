f9b81a493a026e6060959a040526af7557587bf5
Adam Gall <adam@decentlabs.io>
2020-03-24 15:21:15 -0400

    Add logo to top section of login page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 
 import BMxLogoTransparent from './assets/images/bmx-logo-transparent.png';
+import BMxLogoColor from './assets/images/bmx-logo-color.svg';
 
 function App() {
   return (
@@ -15,7 +16,11 @@ function App() {
           <Image src={BMxLogoTransparent} fluid className="align-self-end mb-5" />
         </Col>
         <Col className="d-flex flex-column my-3">
-          <Row className="flex-fill" />
+          <Row className="flex-fill mx-auto">
+            <Col className="my-auto">
+              <Image src={BMxLogoColor} />
+            </Col>
+          </Row>
           <Row>
             <Col>
               <p>Hello BlockMedx</p>
