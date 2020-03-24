574da957f6731be44fe38a8fec8a2311cff4a2c0
Adam Gall <adam@decentlabs.io>
2020-03-24 16:54:06 -0400

    Remove app.test.js

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders hello bmx text', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/hello bmx/i);
-  expect(linkElement).toBeInTheDocument();
-});
