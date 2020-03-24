b70fde1ff206ba666014f1f9b8e7ba0ea42109db
Adam Gall <adam@decentlabs.io>
2020-03-24 16:52:40 -0400

    Move the "left hero" styles into App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 
+import styled from 'styled-components'
+
+import BMxLoginSplash from './assets/images/bmx-login-splash.png';
 import BMxLogoTransparent from './assets/images/bmx-logo-transparent.png';
 import BMxLogoColor from './assets/images/bmx-logo-color.svg';
+
+const LeftHero = styled(Col)`
+  background: url(${BMxLoginSplash}) no-repeat center center;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+`;
 
 function App() {
   return (
     <Container fluid className="h-100">
       <Row className="h-100">
-        <Col className="login-splash d-flex">
+        <LeftHero className="d-flex">
           <Image src={BMxLogoTransparent} fluid className="align-self-end mb-5" />
-        </Col>
+        </LeftHero>
         <Col className="d-flex flex-column my-3">
           <Row className="flex-fill mx-auto">
             <Col className="my-auto">
@@ -23,7 +34,7 @@ function App() {
           </Row>
           <Row>
             <Col>
-              <p>Hello BlockMedx</p>
+              <h1>Login</h1>
             </Col>
           </Row>
           <Row className="flex-fill" />
