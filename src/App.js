a468a144be4bb515500a1271e2ffc008eb3e5c45
Adam Gall <adam@decentlabs.io>
2020-03-24 13:26:01 -0400

    Create the half-screen background image container on login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+import BMxLogoTransparent from './assets/images/bmx-logo-transparent.png';
 
 function App() {
   return (
     <Container fluid className="h-100">
-      Hello BlockMedx
+      <Row className="h-100">
+        <Col className="login-splash d-flex">
+          <Image src={BMxLogoTransparent} fluid className="align-self-end mb-5" />
+        </Col>
+        <Col>
+          Hello BlockMedx
+        </Col>
+      </Row>
     </Container>
   );
 }
