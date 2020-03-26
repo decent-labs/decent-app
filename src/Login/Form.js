152b5eb98e847dc35151f5fe96ff8873c15a6962
Adam Gall <adam@decentlabs.io>
2020-03-26 16:53:44 -0400

    Keep passing the authSuccess function down into the actual Form where API request happens

--- src/Login/Form.js
+++ src/Login/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-function LoginForm() {
+function LoginForm({ authSuccess }) {
   return (
     <>
       <Form>
