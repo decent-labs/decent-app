1e2d4be4a9613265182c45c69e780fe6250c1fe9
Adam Gall <adam@decentlabs.io>
2020-03-31 00:01:12 -0400

    Add a Router to App with authed and unauthed routes for Auth and Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import {
+  BrowserRouter as Router,
+  Switch
+} from 'react-router-dom';
+
 import Auth from './Auth';
+import Home from './Home';
+import PrivateRoute from './Auth/PrivateRoute';
+import UnauthedRoute from './Auth/UnauthedRoute';
 
 function App() {
   return (
-    <Auth />
+    <Router>
+      <Switch>
+        <UnauthedRoute path='/auth'>
+          <Auth />
+        </UnauthedRoute>
+        <PrivateRoute exact path='/'>
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </Router>
   );
 }
 
