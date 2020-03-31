b67b1efb782008e0326169dc0c7d7d22792cb996
Adam Gall <adam@decentlabs.io>
2020-03-31 02:07:26 -0400

    Create the Password Recovery form which takes an email address, then redirect back to login page on success

--- src/Auth/Forgot/index.js
+++ src/Auth/Forgot/index.js
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function ForgotPassword() {
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
+import { Link, useHistory } from 'react-router-dom';
+
+import { resetPassword } from '../../requests';
+
+function ForgotPassword({ alert }) {
+  const history = useHistory();
+
+  const [email, setEmail] = useState('');
+
+  const [error, setError] = useState('');
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const submitForm = () => {
+    setError('');
+    setButtonDisabled(true);
+
+    return resetPassword(email)
+      .then(_ => {
+        setButtonDisabled(false);
+        setEmail('');
+        alert({message: 'Password reset link sent to email', variant: 'primary'})
+        history.push('/auth/login')
+      })
+      .catch(error => {
+        setButtonDisabled(false);
+        setEmail('');
+        setError(error);
+      });
+  }
+
   return (
-    <h1>Forgot Password</h1>
+    <Col>
+      <h1>Password Recovery</h1>
+      <h4>Need help signing in?</h4>
+      <p>We'll need you to provide your email address so we can send you a password reset link.</p>
+      <Form onSubmit={event => event.preventDefault()}>
+        <Form.Group controlId='formEmail'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='you@email.com'
+            autoComplete='username'
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId='formError'>
+          {error && <Alert variant='danger'>{error}</Alert>}
+        </Form.Group>
+        <Button
+          variant='primary'
+          type='submit'
+          block
+          className='font-weight-bold'
+          disabled={buttonDisabled}
+          onClick={submitForm}
+        >
+          Send Link
+        </Button>
+      </Form>
+      <div className='text-center my-3'>
+        <Link to='/auth/login'>Back to Login</Link>
+      </div>
+    </Col>
   );
 }
 
