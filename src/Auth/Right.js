0347480b1d3cfe291634b5ef129d39c85331c733
Adam Gall <adam@decentlabs.io>
2020-03-31 00:06:17 -0400

    Add Switch routing to Auth/Right.js for /auth/login and redirection to that if user goes to /auth

--- src/Auth/Right.js
+++ src/Auth/Right.js
@@ -3,11 +3,19 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import {
+  Switch,
+  Route,
+  Redirect,
+  useRouteMatch
+} from 'react-router-dom';
 
 import BMxLogoColor from '../assets/images/bmx-logo-color.svg';
 import Login from './Login';
 
 function Right() {
+  const match = useRouteMatch();
+
   return (
     <Col className='d-flex flex-column my-3'>
       <Row className='flex-fill mx-auto'>
@@ -16,7 +24,14 @@ function Right() {
         </Col>
       </Row>
       <Row className='mx-5'>
-        <Login />
+        <Switch>
+          <Route path={`${match.path}/login`}>
+            <Login />
+          </Route>
+          <Route path={match.path}>
+            <Redirect replace to={`${match.path}/login`} />
+          </Route>
+        </Switch>
       </Row>
       <Row className='flex-fill' />
     </Col>
