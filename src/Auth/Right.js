aa4bdbe9b009bee12d9c1517951aa86a9d3cf7d1
Adam Gall <adam@decentlabs.io>
2020-03-31 11:22:30 -0400

    Create route for Reset Password page

--- src/Auth/Right.js
+++ src/Auth/Right.js
@@ -14,6 +14,7 @@ import {
 import BMxLogoColor from '../assets/images/bmx-logo-color.svg';
 import Login from './Login';
 import Forgot from './Forgot';
+import Reset from './Reset';
 
 function Right() {
   const match = useRouteMatch();
@@ -45,6 +46,9 @@ function Right() {
           </Route>
           <Route path={`${match.path}/forgot`}>
             <Forgot alert={setAlert} />
+          </Route>
+          <Route path={`${match.path}/reset`}>
+            <Reset alert={setAlert} />
           </Route>
           <Route path={match.path}>
             <Redirect replace to={`${match.path}/login`} />
