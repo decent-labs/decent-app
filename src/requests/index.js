2786b058f46f778e9562735234cc324a96038c75
Adam Gall <adam@decentlabs.io>
2020-04-03 16:31:46 -0400

    Remove the "login" request function and replace it with a custom hook called useFetch

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const basicHeaders = new Headers({
   'Content-Type': 'application/json',
   'Authorization': `Basic ${btoa(`${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_CLIENT_SECRET}`)}`
@@ -32,14 +34,6 @@ function processResponse(response) {
   });
 }
 
-function login(email, password, token) {
-  return new Promise((resolve, reject) => {
-    return makeRequest('auth/login', 'POST', basicHeaders, { email, password, token })
-      .then(response => resolve(response))
-      .catch(error => reject(error));
-  });
-}
-
 function forgotPassword(email) {
   return new Promise((resolve, reject) => {
     return makeRequest('password', 'POST', basicHeaders, { email })
@@ -58,4 +52,59 @@ function resetPassword(email, token, newPassword, twoFAToken) {
   });
 }
 
-export { login, forgotPassword, resetPassword };
+const useFetch = () => {
+  const [fetchedData, setFetchedData] = useState(null);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sendRequest = (path, method, body) => () => {
+    const basicHeaders = new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${btoa(`${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_CLIENT_SECRET}`)}`
+    });
+
+    const options = {
+      method: method,
+      headers: basicHeaders
+    };
+
+    if (body) options.body = JSON.stringify(body);
+    if (isLoading) return;
+
+    setIsLoading(true);
+    setError(false);
+
+    fetch(`${process.env.REACT_APP_API_URL}/${path}`, options)
+      .then(response => {
+        if (!response.ok) {
+          response.json()
+            .then(error => {
+              setIsLoading(false);
+              setError(error.error);
+            })
+            .catch(error => {
+              setIsLoading(false);
+              setError(error.message)
+            })
+        } else {
+          response.json()
+            .then(response => {
+              setIsLoading(false);
+              setFetchedData(response);
+            })
+            .catch(error => {
+              setIsLoading(false);
+              setError(error.message);
+            });
+        }
+      })
+      .catch(error => {
+        setIsLoading(false);
+        setError(error.message);
+      });
+  };
+
+  return [sendRequest, isLoading, fetchedData, error];
+}
+
+export { useFetch, forgotPassword, resetPassword };
