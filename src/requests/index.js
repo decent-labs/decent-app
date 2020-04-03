c7ecc2333f50afbd0bbd8a1f5e52ca0d36e72d89
Adam Gall <adam@decentlabs.io>
2020-04-03 16:51:56 -0400

    Remove the 'resetPassword' request function, and all the other functions that it depended on which are no longer used

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,49 +1,5 @@
 import { useState } from 'react';
 
-const basicHeaders = new Headers({
-  'Content-Type': 'application/json',
-  'Authorization': `Basic ${btoa(`${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_CLIENT_SECRET}`)}`
-});
-
-function makeRequest(route, method, headers, body = null) {
-  const options = {
-    method: method,
-    headers: headers
-  };
-  if (body) options.body = JSON.stringify(body);
-  return new Promise((resolve, reject) => {
-    return fetch(`${process.env.REACT_APP_API_URL}/${route}`, options)
-      .then(response => {
-        return processResponse(response)
-          .then(response => resolve(response))
-          .catch(error => reject(error))
-      })
-      .catch(error => reject(error.message));
-  });
-}
-
-function processResponse(response) {
-  return new Promise((resolve, reject) => {
-    if (!response.ok) {
-      return response.json()
-        .then(error => reject(error.error));
-    }
-    return response.json()
-      .then(response => resolve(response))
-      .catch(error => reject(error.message));
-  });
-}
-
-function resetPassword(email, token, newPassword, twoFAToken) {
-  return new Promise((resolve, reject) => {
-    return makeRequest('password', 'PUT', basicHeaders, {
-      email, token, newPassword, twoFAToken
-    })
-      .then(response => resolve(response))
-      .catch(error => reject(error));
-  });
-}
-
 const useFetch = () => {
   const [fetchedData, setFetchedData] = useState(null);
   const [error, setError] = useState(false);
@@ -99,4 +55,4 @@ const useFetch = () => {
   return [sendRequest, isLoading, fetchedData, error];
 }
 
-export { useFetch, resetPassword };
+export { useFetch };
