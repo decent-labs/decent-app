ac9b190e582902ee485df476d4175a5e5be296a7
Adam Gall <adam@decentlabs.io>
2020-04-06 15:03:49 -0400

    Move the left bar into a new component

---
+++ src/Home/Left/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
+import { useCookies } from 'react-cookie';
+
+import BMxLogoColor from '../../assets/images/bmx-logo-color.svg';
+
+function Left() {
+  const [, , removeCookie] = useCookies(['token']);
+
+  return (
+    <>
+      <Row className='mx-auto'>
+        <Col>
+          <Image fluid src={BMxLogoColor} />
+        </Col>
+      </Row>
+      <Row className='mt-5 mb-4 pb-5 border-bottom'>
+        <Col>
+          <Dropdown className="shadow rounded">
+            <Dropdown.Toggle block variant="secondary" id="dropdown-basic">
+              Select Profile
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Patient</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Prescriber</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
+      <Row className='mx-auto'>
+        <Col>
+          <Button
+            variant='link'
+            onClick={() => removeCookie('token')}
+          >
+            Log Out
+          </Button>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default Left;
--- src/Home/index.js
+++ src/Home/index.js
@@ -3,48 +3,17 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Button from 'react-bootstrap/Button';
-import { useCookies } from 'react-cookie';
 
-import BMxLogoColor from '../assets/images/bmx-logo-color.svg';
+import Left from './Left';
 
 function Home() {
-  const [, , removeCookie] = useCookies(['token']);
+  
 
   return (
     <Container fluid className='h-100 main-container'>
       <Row className='h-100'>
         <Col xs={4} md={3} lg={2} className='py-5 shadow bg-light d-flex flex-column'>
-          <Row className='mx-auto'>
-            <Col>
-              <Image fluid src={BMxLogoColor} />
-            </Col>
-          </Row>
-          <Row className='mt-5 mb-4 pb-5 border-bottom'>
-            <Col>
-              <Dropdown className="shadow rounded">
-                <Dropdown.Toggle block variant="secondary" id="dropdown-basic">
-                  Select Profile
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1">Patient</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">Prescriber</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Col>
-          </Row>
-          <Row className='mx-auto'>
-            <Col>
-              <Button
-                variant='link'
-                onClick={() => removeCookie('token')}
-              >
-                Log Out
-              </Button>
-            </Col>
-          </Row>
+          <Left />
         </Col>
         <Col xs={8} md={7} lg={10}>
           
