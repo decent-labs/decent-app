83dc00dad7940e38676d8d096a7909e786f0b9c2
Adam Gall <adam@decentlabs.io>
2020-04-07 14:56:15 -0400

    Clean up formatting in NavMenu

--- src/Home/NavMenu/index.js
+++ src/Home/NavMenu/index.js
@@ -17,19 +17,32 @@ function NavMenu() {
   const [, , removeCookie] = useCookies(['token']);
 
   return (
-    <Navbar bg="d-flex">
-      <Form className="flex-grow-1 mr-4">
+    <Navbar bg='d-flex'>
+      <Form className='flex-grow-1 mr-4'>
         <InputGroup>
           <InputGroup.Prepend>
-            <InputGroup.Text className="bg-transparent border-right-0"><Image src={BMxSearchIcon} /></InputGroup.Text>
+            <InputGroup.Text className='bg-transparent border-right-0'>
+              <Image src={BMxSearchIcon} />
+            </InputGroup.Text>
           </InputGroup.Prepend>
           <Form.Control type='text' placeholder='Search by name' className='py-2 border-left-0 border' />
         </InputGroup>
       </Form>
       <Nav>
-        <Nav.Link href="#notifications"><Image src={BMxNotificationIcon} /></Nav.Link>
-        <NavDropdown title={<><Image src={BMxProfileIcon} />&nbsp;&nbsp;My Account</>} id="basic-nav-dropdown" alignRight>
-          <NavDropdown.Item href="#action/3.1">Account Details</NavDropdown.Item>
+        <Nav.Link href='#notifications'>
+          <Image src={BMxNotificationIcon} />
+        </Nav.Link>
+        <NavDropdown
+          title={
+            <>
+              <Image src={BMxProfileIcon} />
+              <span className='ml-3'>My Account</span>
+            </>
+          }
+          id='basic-nav-dropdown'
+          alignRight
+        >
+          <NavDropdown.Item href='#me'>Account Details</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item onClick={() => removeCookie('token')}>
             Log Out
