2df92d898738fbc9fa899bdb6bf9e676c82024f6
Adam Gall <adam@decentlabs.io>
2020-04-07 14:29:37 -0400

    Fix an incorrect breakpoint column on the main screen and add some padding

--- src/Home/index.js
+++ src/Home/index.js
@@ -15,7 +15,7 @@ function Home() {
         <Col xs={4} md={3} lg={2} className='py-5 shadow bg-light d-flex flex-column'>
           <Left />
         </Col>
-        <Col xs={8} md={7} lg={10}>
+        <Col xs={8} md={9} lg={10} className='py-4'>
           
         </Col>
       </Row>
