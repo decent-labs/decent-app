5974ae88fe9b63fd95faf0fc155c6d152204f8ae
Adam Gall <adam@decentlabs.io>
2020-04-14 16:28:03 -0400

    Add alert to the Oauth Manager index page, and pass it into the New component

--- src/Settings/OauthManager/index.js
+++ src/Settings/OauthManager/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import { Switch, Route, Redirect, useRouteMatch, Link } from 'react-router-dom';
 
@@ -8,22 +9,35 @@ import New from './New';
 
 function OauthManager() {
   const match = useRouteMatch();
+  const [alert, setAlert] = useState(null);
 
   return (
     <>
+      {alert &&
+        <Alert
+          className='mt-3'
+          variant={alert.variant}
+          dismissible
+          onClose={() => setAlert(null)}
+        >
+          {alert.message}
+        </Alert>
+      }
+
       <Route exact path={`${match.path}`}>
         <Link to={`${match.path}/new`} className='float-right'>
           <Button>New Application</Button>
         </Link>
       </Route>
+
       <Switch>
         <Route path={`${match.path}/new/:unknown`}>
           <Redirect to={`${match.path}/new`} />
         </Route>
         <Route path={`${match.path}/new`}>
-          <New />
+          <New alert={setAlert} />
         </Route>
-        
+
         <Route path={`${match.path}/:unknown`}>
           <Redirect to={`${match.path}`} />
         </Route>
