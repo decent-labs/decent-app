5d8ee2594636005d5bc398b0960c1478140f5f4f
Adam Gall <adam@decentlabs.io>
2020-04-15 00:57:21 -0400

    Create a shared async ADD_DATA action type

--- src/redux/reducers/async/index.js
+++ src/redux/reducers/async/index.js
@@ -1,5 +1,6 @@
 const LOADING = stateProperty => `${stateProperty}/loading`;
 const SET_DATA = stateProperty => `${stateProperty}/setData`;
+const ADD_DATA = stateProperty => `${stateProperty}/addData`;
 const ERROR = stateProperty => `${stateProperty}/error`;
 
 export const dataLoadingAction = stateProperty => ({
@@ -10,6 +11,11 @@ export const dataLoadingAction = stateProperty => ({
 export const dataUpdateAction = (stateProperty, data) => ({
   data,
   type: SET_DATA(stateProperty)
+});
+
+export const dataAddAction = (stateProperty, data) => ({
+  data,
+  type: ADD_DATA(stateProperty)
 });
 
 export const dataLoadingErrorAction = (stateProperty, error) => ({
@@ -28,11 +34,13 @@ export const getAsyncDataReducer = stateProperty => {
   let specificReducerModule = null;
   let specificInitialState = null;
   let specificSetDataReducer = null;
+  let specificAddDataReducer = null;
 
   try {
     specificReducerModule = require(`./${stateProperty}`);
     specificInitialState = specificReducerModule['initialState'];
     specificSetDataReducer = specificReducerModule['setData'];
+    specificAddDataReducer = specificReducerModule['addData'];
   } catch (error) {
     // swallow
     console.log(`couldn't find a specific module or exported functions for ${stateProperty} reducer`)
@@ -56,6 +64,14 @@ export const getAsyncDataReducer = stateProperty => {
           isStale: false,
           error: null,
           data: specificSetDataReducer ? specificSetDataReducer(state.data, action) : action.data
+        };
+      case ADD_DATA(stateProperty):
+        return {
+          ...state,
+          isLoading: false,
+          isStale: false,
+          error: null,
+          data: specificAddDataReducer ? specificAddDataReducer(state.data, action) : action.data
         };
       case ERROR(stateProperty):
         return {
