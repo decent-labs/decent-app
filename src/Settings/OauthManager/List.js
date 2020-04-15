8671d187e19bada6316eae2de583be4c60a92c4c
Adam Gall <adam@decentlabs.io>
2020-04-15 00:59:41 -0400

    Implement a List component that lists out oauth applications

--- src/Settings/OauthManager/List.js
+++ src/Settings/OauthManager/List.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
+import Table from 'react-bootstrap/Table';
+
+import { useAsyncState } from '../../redux/actions/useAsyncState';
+import { StateProperty } from '../../redux/reducers';
+
+import { request } from '../../requests';
 
 function List() {
-  return(
-    <h3>List of OAuth Applications</h3>
+  const oauthAppsLoader = useCallback(() => request('hospitalOrgs/5/oauthApplications', 'GET'), []);
+  const oauthApps = useAsyncState(StateProperty.oauthApps, oauthAppsLoader);
+
+  const appRows = oauthApps.data.map((app, index) => (
+    <tr key={index}>
+      <td>{app.name}</td>
+      <td>{app.redirectURI}</td>
+      <td>{app.clientId}</td>
+      <td>{app.clientSecret}</td>
+    </tr>
+  ))
+
+  return (
+    <>
+      <h3>OAuth Applications</h3>
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Redirect URI</th>
+            <th>Client ID</th>
+            <th>Client Secret</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appRows}
+        </tbody>
+      </Table>
+    </>
   );
 }
 
