69e2728ec9aca797e7b5a864692e7858e02a67c0
Michael Ngo <michael.ngo@decentlabs.io>
2020-04-15 17:06:13 -0400

    Select a profile, display a message
    Signed-off-by: Michael Ngo <michael.ngo@decentlabs.io>

--- src/Welcome/index.js
+++ src/Welcome/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import {useAsyncState} from "../redux/actions/useAsyncState";
+import {StateProperty} from "../redux/reducers";
 
 function Welcome() {
+  const userProfile = useAsyncState(StateProperty.userProfile);
+
   return (
-    <div>Welcome</div>
+    <div>Welcome {userProfile.data.currentProfile.profileType}</div>
   );
 }
 
