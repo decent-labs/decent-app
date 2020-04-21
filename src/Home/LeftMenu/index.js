e8b29ed26dda6eb199b6bfb9c505b170cde31ecc
Michael Ngo <michael.ngo@decentlabs.io>
2020-04-21 18:07:25 -0400

    remove unnecessary function

--- src/Home/LeftMenu/index.js
+++ src/Home/LeftMenu/index.js
@@ -34,22 +34,12 @@ function LeftMenu() {
     dispatch(dataUpdateAction(StateProperty.userProfile, {currentProfile, profiles:userProfiles.data.profiles}));
     history.push('/');
   }
-
-  function handleNavigation(page) {
-    switch (page) {
-      case 'patients':
-        history.push('/patients');
-        break;
-      default:
-        history.push('/');
-    }
-  }
-
+  
   return (
     <>
       <Row className='mx-auto pt-2'>
         <Col>
-          <Link to='' onClick={() => handleNavigation('/')}>
+          <Link to='' onClick={() => history.push('/')}>
             <Image fluid src={BMxLogoColor} />
           </Link>
         </Col>
