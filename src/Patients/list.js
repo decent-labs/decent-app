95b698342dbaca2cf5e11cf3e96bce456b8fc501
Michael Ngo <michael.ngo@decentlabs.io>
2020-04-23 16:28:21 -0400

    some styling and list patients
    list patients based on patient or prescriber user types

--- src/Patients/list.js
+++ src/Patients/list.js
@@ -3,38 +3,54 @@ import {Col, Row, Table} from "react-bootstrap";
 import {useAsyncState} from "../redux/actions/useAsyncState";
 import {StateProperty} from "../redux/reducers";
 import {request} from "../requests";
+import {Link} from "react-router-dom";
 
 function List() {
   const userProfiles = useAsyncState(StateProperty.userProfile);
-  const patientsLoader = useCallback(() => {
+  const patientsLoader = useCallback(async () => {
     let patients;
       if(userProfiles.data.currentProfile.profileType === 'patient') {
         patients = userProfiles.data.profiles
           .filter(curProfile => curProfile.profileType === 'patient')
-          .map(curProfile => request(`patients/${curProfile.profileId}/profile`, 'GET'));
+          .map(curPatient => request(`patients/${curPatient.profileId}/profile`, 'GET'));
       }else if(userProfiles.data.currentProfile.profileType === 'prescriber') {
-        patients = request(`prescribers/${userProfiles.data.currentProfile.profileId}/patients`, 'GET')
-          .then(results =>
-            results.patients.map(curPatient => request(`patients/${curPatient.patientId}/profile`, 'GET'))
-          )
+        let patientProfiles = await request(`prescribers/${userProfiles.data.currentProfile.profileId}/patients`, 'GET')
+        patients = patientProfiles.patients.map(curPatient => request(`patients/${curPatient.patientId}/profile`, 'GET'))
       }
 
-      console.log('patients ',patients);
       return Promise.all(patients)
         .then(profiles => profiles.map(curProfile => curProfile.profile))
-        .then(values => values)
     },
     [userProfiles.data.profiles, userProfiles.data.currentProfile]);
   const patients = useAsyncState(StateProperty.patients, patientsLoader);
 
-  const patientRows = patients.data.patients.map((patient, index) =>
-    <tr key={index}>
-      <td>{patient.lastName}</td>
-      <td>{patient.firstName}</td>
-      <td>{patient.dob}</td>
-      <td>{}</td>
-      <td>{}</td>
-    </tr>
+  const patientRows = patients.data.patients.map((patient, index) =>{
+    const dob = new Date(patient.dob)
+
+    return (
+      <tr key={index}>
+        <td>{patient.lastName}</td>
+        <td>{patient.firstName}</td>
+        <td>{(`${dob.getMonth()}/${dob.getDay()}/${dob.getFullYear()}`)}</td>
+        <td>{}</td>
+        <td className='action-items'>
+          <Link to=''>
+            <div>
+              <svg className="bi bi-eye-fill" width="1em" height="1em"
+                    viewBox="0 0 16 16" fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg">
+                <path d="M10.5 8a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z"/>
+                <path fillRule="evenodd"
+                  d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 100-7 3.5 3.5 0 000 7z"
+                  clipRule="evenodd"/>
+              </svg>
+            </div>
+          </Link>
+        </td>
+      </tr>
+    )
+  }
+
   )
 
   return(
