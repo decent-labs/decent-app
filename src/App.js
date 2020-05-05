5a052c45d2e203d8db202c64329c57d4de183b74
Adam Gall <adam@decentlabs.io>
2020-05-05 15:42:46 -0400

    Move the /auth/me and /auth/profiles requests out of App.js and into Home/index.js, so that they're executed when the user is authenticated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 
 import {
   BrowserRouter as Router,
@@ -9,24 +9,9 @@ import Auth from './Auth';
 import Home from './Home';
 import PrivateRoute from './Routes/PrivateRoute';
 import UnauthedRoute from './Routes/UnauthedRoute';
-import {request} from "./requests";
-import {useAsyncState} from "./redux/actions/useAsyncState";
-import {StateProperty} from "./redux/reducers";
+
 
 function App() {
-  const accountLoader = useCallback(() => request('auth/me', 'GET'), []);
-  useAsyncState(StateProperty.account, accountLoader);
-  const userProfileLoader = useCallback(() => {
-    return request('auth/profiles', 'GET')
-      .then((response) => {
-        return {
-          currentProfile: response.profiles[0],
-          profiles: response.profiles
-        }
-      })
-  }, []);
-  useAsyncState(StateProperty.userProfile, userProfileLoader);
-
   return (
     <Router>
       <Switch>
