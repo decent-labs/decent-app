979e58cb0463b0d4ad1b8bbadd0d6a2e46599612
DrNgo <michael.ngo@decentlabs.io>
2020-06-04 16:01:08 -0400

    standardized forms, missed lab agent (#102)

--- src/Labs/invite.js
+++ src/Labs/invite.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Container, Form, Row} from "react-bootstrap";
+import {Col, Container, Form, Row} from "react-bootstrap";
 import {request} from "../requests";
 import Button from "react-bootstrap/Button";
 import {useHistory, useParams} from "react-router-dom";
@@ -40,84 +40,86 @@ function Invite({alert}) {
   return (
     <>
       <Container>
-        <Row className='justify-content-center'>
-          <h2>Invite Lab Agent</h2>
-        </Row>
-        <Form onSubmit={sendRequest}>
-          <Form.Group className='required'>
-            <Form.Label>First Name</Form.Label>
-            <Form.Control
-              value={firstName}
-              onChange={event => setFirstName(event.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group className='required'>
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control
-              value={lastName}
-              onChange={event => setLastName(event.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group className='required'>
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type='email'
-              value={email}
-              onChange={event => setEmail(event.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group className='required'>
-            <Form.Label>Date of Birth</Form.Label>
-            <Form.Control
-              type='date'
-              placeholder='mm/dd/yy'
-              autoComplete='dob'
-              value={dob}
-              onChange={event => setDob(event.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group className='required'>
-            <Form.Label>License Number</Form.Label>
-            <Form.Control
-              value={licenseNumber}
-              onChange={event => setLicenseNumber(event.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group className='required'>
-            <Form.Label>State of License</Form.Label>
-            <Form.Control as='select'
-                          defaultValue='Select'
-                          onChange={event => setStateOfLicense(event.target.value)}
-            >
-              {getStateOptions()}
-            </Form.Control>
-          </Form.Group>
-          <Form.Group className='required'>
-            <Form.Label>Organization Name</Form.Label>
-            <Form.Control
-              value={organizationName}
-              onChange={event => setOrganizationName(event.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Row className='pt-2 justify-content-center'>
-            <Form.Group controlId='formSubmit'>
-              <Button
-                variant='primary'
-                type='submit'
-                block
-                className='font-weight-bold'
-              >
-                Invite
-              </Button>
+        <Col lg={6} md={8}>
+          <Row>
+            <h2>Invite Lab Agent</h2>
+          </Row>
+          <Form onSubmit={sendRequest}>
+            <Form.Group className='required'>
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                value={firstName}
+                onChange={event => setFirstName(event.target.value)}
+                required
+              />
             </Form.Group>
-          </Form.Row>
-        </Form>
+            <Form.Group className='required'>
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                value={lastName}
+                onChange={event => setLastName(event.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group className='required'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type='email'
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group className='required'>
+              <Form.Label>Date of Birth</Form.Label>
+              <Form.Control
+                type='date'
+                placeholder='mm/dd/yy'
+                autoComplete='dob'
+                value={dob}
+                onChange={event => setDob(event.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group className='required'>
+              <Form.Label>License Number</Form.Label>
+              <Form.Control
+                value={licenseNumber}
+                onChange={event => setLicenseNumber(event.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group className='required'>
+              <Form.Label>State of License</Form.Label>
+              <Form.Control as='select'
+                            defaultValue='Select'
+                            onChange={event => setStateOfLicense(event.target.value)}
+              >
+                {getStateOptions()}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group className='required'>
+              <Form.Label>Organization Name</Form.Label>
+              <Form.Control
+                value={organizationName}
+                onChange={event => setOrganizationName(event.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Row className='pt-2 justify-content-end'>
+              <Form.Group controlId='formSubmit'>
+                <Button
+                  variant='primary'
+                  type='submit'
+                  block
+                  className='font-weight-bold'
+                >
+                  Invite
+                </Button>
+              </Form.Group>
+            </Form.Row>
+          </Form>
+        </Col>
       </Container>
     </>
   )
