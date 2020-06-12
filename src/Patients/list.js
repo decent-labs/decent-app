4fe82925308e6b69754ef20d661ab16304dc6865
Dante Haskell Elrik <elrikdante@users.noreply.github.com>
2020-06-12 16:46:17 -0400

    2020-06-12.1591989216: adds pagination controls for remaining patient types (#152)

--- src/Patients/list.js
+++ src/Patients/list.js
@@ -8,6 +8,17 @@ import {getPrescriptionData} from "../Common/form";
 
 function List() {
   const [currentPage, setCurrentPage] = useState(1);
+  const [paginationData, setPaginationData] = useState({
+      currentPage: 0,
+      from: 0,
+      lastPage: 0,
+      perPage: 0,
+      to: 0,
+      total: 0,
+      disabled: true
+  });
+  const lastPage = parseInt(paginationData.lastPage);
+  const pdCurrentPage = parseInt(paginationData.currentPage);
   const userProfiles = useAsyncState(StateProperty.userProfile);
   const patientsLoader = useCallback(async () => {
       let patients;
@@ -16,11 +27,13 @@ function List() {
           .filter(curProfile => curProfile.profileType === 'patient')
           .map(curPatient => request(`patients/${curPatient.profileId}/profile`, 'GET'));
       }else if(userProfiles.data.currentProfile.profileType === 'prescriber') {
-        let patientProfiles = await request(`prescribers/${userProfiles.data.currentProfile.profileId}/patients`, 'GET')
+        let patientProfiles = await request(`prescribers/${userProfiles.data.currentProfile.profileId}/patients?currentPage=${currentPage}&perPage=10`, 'GET')
         patients = patientProfiles.patients.map(curPatient => request(`patients/${curPatient.patientId}/profile`, 'GET'))
+        setPaginationData(patientProfiles.pagination)
       }else if(['internal','labOrg', 'labAgent'].includes(userProfiles.data.currentProfile.profileType)) {
         let patientProfiles = await request(`patients/list?currentPage=${currentPage}`, 'GET')
         patients = patientProfiles.profile.data.map(curPatient => request(`patients/${curPatient.id}/profile`, 'GET'))
+        setPaginationData(patientProfiles.profile.pagination);
       }
 
       return getPrescriptionData(patients);
@@ -32,12 +45,15 @@ function List() {
     if(patients.data.length > 0)
       return <>
         <ListTable patients={patients.data}></ListTable>
-        {userProfiles.data.currentProfile.profileType === 'internal' && (
+        {
+         ['internal','labOrg', 'labAgent', 'prescriber'].includes(userProfiles.data.currentProfile.profileType)
+         && !paginationData.disabled
+         && (
           <Pagination as={'Container'} className='justify-content-end'>
-            <Pagination.First onClick={() => setCurrentPage(1)}/>
-            <Pagination.Prev onClick={() => setCurrentPage(currentPage - 1)}/>
+            { pdCurrentPage > 1 && <Pagination.First onClick={() => setCurrentPage(1)}/> }
+            { pdCurrentPage > 1 && <Pagination.Prev onClick={() => setCurrentPage(currentPage - 1)}/> }
             <Pagination.Item active>{currentPage}</Pagination.Item>
-            <Pagination.Next onClick={() => setCurrentPage(currentPage + 1)}/>
+            {pdCurrentPage < lastPage && <Pagination.Next onClick={() => setCurrentPage(currentPage + 1)}/> }
           </Pagination>
         )}
       </>
