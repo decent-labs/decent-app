7e5fc76871602478693b5af1318f1a9a0ee55696
DrNgo <michael.ngo@decentlabs.io>
2020-06-17 12:23:18 -0400

    remove required from notes in new prescription (#198)
    * remove required from notes in new prescription
    
    * remove container element to fix spacing

--- src/Prescriptions/new.js
+++ src/Prescriptions/new.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Col, Container, Form, Row} from "react-bootstrap";
+import {Col, Form, Row} from "react-bootstrap";
 import {request} from "../requests";
 import Button from "react-bootstrap/Button";
 import {useHistory, useParams} from "react-router-dom";
@@ -43,117 +43,114 @@ function New({alert}) {
 
   return (
     <>
-      <Container>
-        <Col lg={6} md={8}>
-          <Row>
-            <h2>New Test Kit</h2>
-          </Row>
-          <Form onSubmit={sendRequest}>
-            {/*<Form.Row>*/}
-            {/*  <Form.Group className='required' as={Col}>*/}
-            {/*    <Form.Label>Select item</Form.Label>*/}
-            {/*    <Form.Control as="select"*/}
-            {/*                  onChange={event => setDrugInfo(event.target.value)}*/}
-            {/*                  required>*/}
-            {/*      <option value='covid-19-test'>covid-19 test</option>*/}
-            {/*    </Form.Control>*/}
-            {/*  </Form.Group>*/}
-            {/*</Form.Row>*/}
-            {/*<Form.Row className='justify-content-center'>*/}
-            {/*  <Form.Group className='required' lg={6} md={8} as={Col}>*/}
-            {/*    <Form.Label>Expiration Date</Form.Label>*/}
-            {/*    <DatePicker*/}
-            {/*      selected={expirationDate}*/}
-            {/*      onChange={date => setExpirationDate(date)}*/}
-            {/*      required*/}
-            {/*    />*/}
-            {/*  </Form.Group>*/}
-            {/*</Form.Row>*/}
-            {/*<Form.Row className='justify-content-center'>*/}
-            {/*  <Form.Group className='required' lg={3} md={4} as={Col}>*/}
-            {/*    <Form.Label>Detail</Form.Label>*/}
-            {/*    <Form.Control*/}
-            {/*      value={detail}*/}
-            {/*      onChange={event => setDetail(event.target.value)}*/}
-            {/*      required*/}
-            {/*    >*/}
+      <Col lg={6} md={8}>
+        <Row>
+          <h2>New Test Kit</h2>
+        </Row>
+        <Form onSubmit={sendRequest}>
+          {/*<Form.Row>*/}
+          {/*  <Form.Group className='required' as={Col}>*/}
+          {/*    <Form.Label>Select item</Form.Label>*/}
+          {/*    <Form.Control as="select"*/}
+          {/*                  onChange={event => setDrugInfo(event.target.value)}*/}
+          {/*                  required>*/}
+          {/*      <option value='covid-19-test'>covid-19 test</option>*/}
+          {/*    </Form.Control>*/}
+          {/*  </Form.Group>*/}
+          {/*</Form.Row>*/}
+          {/*<Form.Row className='justify-content-center'>*/}
+          {/*  <Form.Group className='required' lg={6} md={8} as={Col}>*/}
+          {/*    <Form.Label>Expiration Date</Form.Label>*/}
+          {/*    <DatePicker*/}
+          {/*      selected={expirationDate}*/}
+          {/*      onChange={date => setExpirationDate(date)}*/}
+          {/*      required*/}
+          {/*    />*/}
+          {/*  </Form.Group>*/}
+          {/*</Form.Row>*/}
+          {/*<Form.Row className='justify-content-center'>*/}
+          {/*  <Form.Group className='required' lg={3} md={4} as={Col}>*/}
+          {/*    <Form.Label>Detail</Form.Label>*/}
+          {/*    <Form.Control*/}
+          {/*      value={detail}*/}
+          {/*      onChange={event => setDetail(event.target.value)}*/}
+          {/*      required*/}
+          {/*    >*/}
 
-            {/*    </Form.Control>*/}
-            {/*  </Form.Group>*/}
-            {/*  <Form.Group className='required' lg={3} md={4} as={Col}>*/}
-            {/*    <Form.Label>Detail</Form.Label>*/}
-            {/*    <Form.Control as='select'*/}
-            {/*                  required>*/}
+          {/*    </Form.Control>*/}
+          {/*  </Form.Group>*/}
+          {/*  <Form.Group className='required' lg={3} md={4} as={Col}>*/}
+          {/*    <Form.Label>Detail</Form.Label>*/}
+          {/*    <Form.Control as='select'*/}
+          {/*                  required>*/}
 
-            {/*    </Form.Control>*/}
-            {/*  </Form.Group>*/}
-            {/*</Form.Row>*/}
-            {/*<Form.Row className='justify-content-center'>*/}
-            {/*  <Form.Group className='required' lg={6} md={8} as={Col}>*/}
-            {/*    <Form.Label>Diagnosis code</Form.Label>*/}
-            {/*    <Form.Control as='select'*/}
-            {/*                  required>*/}
+          {/*    </Form.Control>*/}
+          {/*  </Form.Group>*/}
+          {/*</Form.Row>*/}
+          {/*<Form.Row className='justify-content-center'>*/}
+          {/*  <Form.Group className='required' lg={6} md={8} as={Col}>*/}
+          {/*    <Form.Label>Diagnosis code</Form.Label>*/}
+          {/*    <Form.Control as='select'*/}
+          {/*                  required>*/}
 
-            {/*    </Form.Control>*/}
-            {/*  </Form.Group>*/}
-            {/*</Form.Row>*/}
-            {/*<Form.Group className='required m-3 justify-content-center' lg={6} md={8} as={Row}>*/}
-            {/*    <Form.Label className='pr-2'>Generic Allowed?</Form.Label>*/}
-            {/*      <Form.Check*/}
-            {/*        type='radio'*/}
-            {/*        label='yes'*/}
-            {/*      />*/}
-            {/*      <Form.Check*/}
-            {/*        type='radio'*/}
-            {/*        label='no'*/}
-            {/*        className='ml-3'*/}
-            {/*      />*/}
-            {/*</Form.Group>*/}
-            {/*<Form.Row className='justify-content-center'>*/}
-            {/*  <Form.Group className='required' lg={6} md={8} as={Col}>*/}
-            {/*    <Form.Label>Pharmacy</Form.Label>*/}
-            {/*    <Form.Control as='select'></Form.Control>*/}
-            {/*  </Form.Group>*/}
-            {/*</Form.Row>*/}
-            <Form.Row>
-              <Form.Group className='required' as={Col}>
-                <Form.Label>Directions</Form.Label>
-                <Form.Control
-                  as='textarea'
-                  placeholder='Directions for patient'
-                  value={directions}
-                  onChange={event => setDirections(event.target.value)}
-                  required
-                ></Form.Control>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group className='required' as={Col}>
-                <Form.Label>Notes</Form.Label>
-                <Form.Control
-                  as='textarea'
-                  placeholder='Additional information'
-                  value={notes}
-                  onChange={event => setNotes(event.target.value)}
-                  required
-                ></Form.Control>
-              </Form.Group>
-            </Form.Row>
-            <Form.Row className='pt-2 justify-content-end'>
-              <Form.Group controlId='formSubmit'>
-                <Button
-                  variant='primary'
-                  type='submit'
-                  block
-                  className='font-weight-bold styled-form-button'
-                >
-                  Save and notify patient
-                </Button>
-              </Form.Group>
-            </Form.Row>
-          </Form>
-        </Col>
-      </Container>
+          {/*    </Form.Control>*/}
+          {/*  </Form.Group>*/}
+          {/*</Form.Row>*/}
+          {/*<Form.Group className='required m-3 justify-content-center' lg={6} md={8} as={Row}>*/}
+          {/*    <Form.Label className='pr-2'>Generic Allowed?</Form.Label>*/}
+          {/*      <Form.Check*/}
+          {/*        type='radio'*/}
+          {/*        label='yes'*/}
+          {/*      />*/}
+          {/*      <Form.Check*/}
+          {/*        type='radio'*/}
+          {/*        label='no'*/}
+          {/*        className='ml-3'*/}
+          {/*      />*/}
+          {/*</Form.Group>*/}
+          {/*<Form.Row className='justify-content-center'>*/}
+          {/*  <Form.Group className='required' lg={6} md={8} as={Col}>*/}
+          {/*    <Form.Label>Pharmacy</Form.Label>*/}
+          {/*    <Form.Control as='select'></Form.Control>*/}
+          {/*  </Form.Group>*/}
+          {/*</Form.Row>*/}
+          <Form.Row>
+            <Form.Group className='required' as={Col}>
+              <Form.Label>Directions</Form.Label>
+              <Form.Control
+                as='textarea'
+                placeholder='Directions for patient'
+                value={directions}
+                onChange={event => setDirections(event.target.value)}
+                required
+              ></Form.Control>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Notes</Form.Label>
+              <Form.Control
+                as='textarea'
+                placeholder='Additional information'
+                value={notes}
+                onChange={event => setNotes(event.target.value)}
+              ></Form.Control>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row className='pt-2 justify-content-end'>
+            <Form.Group controlId='formSubmit'>
+              <Button
+                variant='primary'
+                type='submit'
+                block
+                className='font-weight-bold styled-form-button'
+              >
+                Save and notify patient
+              </Button>
+            </Form.Group>
+          </Form.Row>
+        </Form>
+      </Col>
     </>
   )
 }
